90c209db67605befbad32568950d978ead87482b
nwafor6 <nwaforglory6@gmail.com>
2025-06-22 14:39:45 +0100

    alloed all host

--- backend/settings.py
+++ backend/settings.py
@@ -284,7 +284,8 @@ else:
 
     STATIC_URL = "/static/"
     STATICFILES_DIRS = [BASE_DIR / "static"]
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATIC_ROOT = "/home/mywebsite/public_html/dj/static/"
 
 # Email
 EMAIL_BACKEND = os.getenv("MAIL_DRIVER", default=None)
